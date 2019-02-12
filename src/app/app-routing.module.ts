976f606e43d6eb1ff7cab2f04dd6534dad368f08
felipepereira <felipepereira2697@gmail.com>
2019-02-11 23:02:02 -0200

    Creating the first Angular Route to soccerplayers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SoccerplayersComponent} from './soccerplayers/soccerplayers.component';
 
-const routes: Routes = [];
+/*  Defining the routes --> Routes tell the router which view to display when a user 
+    clicks a link or pastes a URL into the browser address bar.
+*/
+const routes: Routes = [
+  {path: 'soccerplayers', component: SoccerplayersComponent}
+] 
+  
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [ RouterModule ]
+  
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule 
+{
+}
