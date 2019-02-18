3e2c184b636ece8c7094a91001523abdc4912163
felipepereira <felipepereira2697@gmail.com>
2019-02-18 16:14:42 -0300

    Converting the mock data to http client

--- src/app/soccerplayer.service.ts
+++ src/app/soccerplayer.service.ts
@@ -3,6 +3,7 @@ import { Soccerplayer } from './soccerplayer';
 import { PLAYERS } from './mock-players';
 import { Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 /*
   The soccerplayer.service could get hero data from anywhere—a web service, local storage, or a mock data source.
 
@@ -13,19 +14,28 @@ service will provide an injectable service */
   providedIn: 'root'
 })
 export class SoccerplayerService {
-
+  private soccerplayersUrl = 'api/soccerplayers';  // URL to web api
   /*
     Here we have a typical service-in-service scenario, so we inject message service into soccer player service that will
     be injected in soccerplayercomponent
   */
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService)
+  { 
+
+  }
+
+  private log(message: string) 
+  {
+    this.messageService.add(`Soccerplayer service: ${message}`);
+  }
   //Using RXJs here to return an Observable
   getSoccerPlayers(): Observable<Soccerplayer[]>
   {
     //returning the mock data
     //After soccerplayers are fetched, send a message
     this.messageService.add('Soccerplayers Service says: Nice! Soccerplayers fetched');
-    return of(PLAYERS);
+    //return of(PLAYERS);
+    return this.http.get<Soccerplayer[]>(this.soccerplayersUrl);
   }
 
   getSoccerPlayer(id: Number): Observable<Soccerplayer>
